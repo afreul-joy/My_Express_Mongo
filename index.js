e543a85958a1cf890f0085cb9665fcb4688d43d9
afreuljoy <afreuljoy@gmail.com>
2023-05-06 23:51:21 +0600

    connected to the dbms

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-const express = require('express');
-
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/testProductDB");
+    console.log("Connected to the database");
+  } catch (error) {
+    console.error("Failed to connect to the database:", error);
+  }
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
 });
 
+app.listen(port, async () => {
+  console.log(`Server is running at http://localhost:${port}`);
+  await connectDB();
+});
