1f4ee80c5eaa10e03e18ec6e31d6653ba0030953
afreuljoy <afreuljoy@gmail.com>
2023-05-07 01:16:06 +0600

    post in database successs

--- index.js
+++ index.js
@@ -4,14 +4,64 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use(express.json()); // for handling json data use middleware
+app.use(express.urlencoded({extended: true})); 
+
+// create product schema
+const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },    
+  description: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// create product model
+const products = mongoose.model("products", productSchema); // products is collection name
+
+// connecting to database
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/testProductDB");
+    await mongoose.connect("mongodb://127.0.0.1:27017/testProductDB"); // testproductDb is database name
     console.log("Connected to the database");
   } catch (error) {
     console.error("Failed to connect to the database:", error);
   }
 };
+
+app.post("/products", async (req, res) => {
+  try {
+    // get data from request body
+    const title = req.body.title;
+    const price = req.body.price;
+    const description = req.body.description;
+
+    //  storing data in database
+    const newProduct = new products({
+      title: title,
+      price: price,
+      description: description,
+    });
+
+    const productData = await newProduct.save();
+    res.status(201).send(productData);
+  } catch (error) {
+    res.status(500).send({
+      message: error.message,
+    });
+  }
+});
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
@@ -21,3 +71,5 @@ app.listen(port, async () => {
   console.log(`Server is running at http://localhost:${port}`);
   await connectDB();
 });
+
+// Database -> collection ( table) -> document  (record)
