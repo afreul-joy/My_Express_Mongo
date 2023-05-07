913d98cc326ff4954eccc05f65eac5ec3faa15b5
afreuljoy <afreuljoy@gmail.com>
2023-05-07 21:58:33 +0600

    read method update

--- index.js
+++ index.js
@@ -5,15 +5,24 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json()); // for handling json data use middleware
-app.use(express.urlencoded({extended: true})); 
-
+app.use(express.urlencoded({ extended: true }));
 
 /*  #################################
          CONNECTING TO MONGODB
     #################################
 */
 
-//  ***create product schema / STRUCTURE/ SHAPE 
+//--------connecting to database----------
+const connectDB = async () => {
+    try {
+      await mongoose.connect("mongodb://127.0.0.1:27017/testProductDB"); // testproductDb is database name
+      console.log("Connected to the database");
+    } catch (error) {
+      console.error("Failed to connect to the database:", error);
+    }
+  };
+
+//--------create product schema / STRUCTURE/ SHAPE----------
 const productSchema = new mongoose.Schema({
   title: {
     type: String,
@@ -22,7 +31,7 @@ const productSchema = new mongoose.Schema({
   price: {
     type: Number,
     required: true,
-  },    
+  },
   description: {
     type: String,
     required: true,
@@ -33,47 +42,86 @@ const productSchema = new mongoose.Schema({
   },
 });
 
-//  ***create product model or collection/table
+//------------create product model or collection/table--------------
 const products = mongoose.model("products", productSchema); // products is collection/table name
- 
 
-// ***connecting to database
-const connectDB = async () => {
-  try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/testProductDB"); // testproductDb is database name
-    console.log("Connected to the database");
-  } catch (error) {
-    console.error("Failed to connect to the database:", error);
-  }
-};
+
 /*  #################################
         CRUD OPERATION STARTING 
     #################################
 */
 
-// ***CREATE ------------Document/Row/Record
+// ------------CREATE -------------Document/Row/Record
+// POST: /products -> Create a new product
 app.post("/products", async (req, res) => {
   try {
     //  storing data/document in database
     const newProduct = new products({
       title: req.body.title,
-      price:  req.body.price,
+      price: req.body.price,
       description: req.body.description,
     });
- 
-    const productData = await newProduct.save();  // save or insert data in database
+
+    const productData = await newProduct.save(); // save or insert data in database
     res.status(201).send(productData);
   } catch (error) {
     res.status(500).send({
       message: error.message,
     });
-  } 
+  }
 });
 
-// ***READ  ------------Document/Row/Record
-app.get("/products", async (req,res)={
+// *** ------------READ------------Document/Row/Record
+// GET: /products -> Return all the products
+app.get("/products", async (req, res) => {
+  try {
+    const product = await products.find();  
+    if(product) {
+        res.status(200).send({         
+            success : true,         // ***response extra information
+            message: "Return all the product",  
+            data: product
+        });
+    }
+    else {
+        res.status(400).send({
+            success : false, 
+            message: "Product not found"
+        });
+    }
+  } catch (error) {
+    res.status(500).send({
+      message: error.message,
+    });
+  }
+});
 
-})
+// Get: /products/:id -> Return a specific product
+app.get("/products/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const product = await products.findOne({_id:id});   
+    if(product) {
+        res.status(200).send({         
+            success : true,         // ***response extra information
+            message: "Return a single product",  
+            data: product
+        });
+    }
+    else {
+        res.status(400).send({
+            success : false, 
+            message: "Product not found"
+        });
+    }
+  } catch (error) {
+    res.status(500).send({
+      message: error.message,
+    });
+  }
+});
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
@@ -81,7 +129,7 @@ app.get("/", (req, res) => {
 
 app.listen(port, async () => {
   console.log(`Server is running at http://localhost:${port}`);
-  await connectDB();   // Connection function called when server is running
+  await connectDB(); // Connection function called when server is running
 });
 
 // Database -> collection ( table) -> document  (record)
@@ -91,4 +139,3 @@ app.listen(port, async () => {
 // POST: /products -> Create a new product
 // PUT: /products/:id -> Update a specific product based on id
 // DELETE: /products/:id -> Delete a product based on id
-
